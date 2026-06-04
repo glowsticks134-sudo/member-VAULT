c7b9ef3d69a74ed375f8be1adc6c11fb45c492f8
Stichachu13 <glowsticks134@gmail.com>
2026-06-04 16:03:40 -0700

    Add config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+
+BOT_NAME = "Member Depot Bot"
+SERVER_NAME = "Member Depot"
+OWNER = "Stichachu13"
+FOOTER_TEXT = "Member Depot | Made by Stichachu13"
+FOOTER_ICON = "https://cdn.discordapp.com/embed/avatars/0.png"
+
+COLOR_PRIMARY = 0x5865F2
+COLOR_SUCCESS = 0x57F287
+COLOR_ERROR = 0xED4245
+COLOR_WARNING = 0xFEE75C
+COLOR_INFO = 0x5865F2
+
+PREFIX = "!"
+
+ANTINUKE_SETTINGS = {
+    "max_bans": 3,
+    "max_kicks": 3,
+    "max_channel_deletes": 2,
+    "max_role_deletes": 2,
+    "max_webhook_creates": 3,
+    "action_window": 10,
+    "punishment": "ban",
+}
